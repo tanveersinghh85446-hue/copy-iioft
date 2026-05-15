cd31d90e7e23fcf30b2ee15d7e40ab171d926b3f
tanveersinghh85446-hue <tanveersinghh85446@gmail.com>
2026-05-15 12:43:58 +0530

    Optimize Vite build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,34 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+// import tailwindcss from '@tailwindcss/vite'
+// import { visualizer } from 'rollup-plugin-visualizer'
+
+// export default defineConfig({
+//   plugins: [
+//     react(),
+//     tailwindcss(),
+//     visualizer({
+//       open: true,
+//       gzipSize: true,
+//       brotliSize: true
+//     })
+//   ],
+
+//   build: {
+//     rollupOptions: {
+//       output: {
+//         manualChunks: {
+//           react: ['react', 'react-dom'],
+//           router: ['react-router-dom'],
+//           motion: ['framer-motion'],
+//           icons: ['react-icons']
+//         }
+//       }
+//     }
+//   }
+// })
+
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -7,6 +38,7 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+
     visualizer({
       open: true,
       gzipSize: true,
@@ -15,6 +47,10 @@ export default defineConfig({
   ],
 
   build: {
+    cssCodeSplit: false,
+
+    chunkSizeWarningLimit: 1000,
+
     rollupOptions: {
       output: {
         manualChunks: {
